6d8c26f5ce9f9cd697eac01e8ad03f5a5cdb85a1
Peter Stuifzand <peter.stuifzand@gmail.com>
2012-03-30 14:39:32 +0200

    More elaborate example

--- examples/ex1.pl
+++ examples/ex1.pl
@@ -6,20 +6,28 @@ use IO::String;
 
 use MarpaX::Parser::Loader 'load_parser';
 
-my $grammar_fh = IO::String->new(<<'INPUT');
+my $grammar_fh = IO::String->new(<<'GRAMMAR');
 NUMBER = /(\d+)/
 SPACE  = /[ \t\r\n]/
 
-Parser  ::= Numbers+   {{ shift; return \@_; }}
-Numbers ::= NUMBER WS  {{ shift; return $_[0]; }}
-WS      ::= SPACE+
-WS      ::= Null
-INPUT
+Parser     ::= Expression+                      {{ shift; return \@_; }}
+Expression ::= WS Term WS $+ WS Expression WS   {{ shift; return $_[1] + $_[5]; }}
+Expression ::= WS Term WS $- WS Expression WS   {{ shift; return $_[1] - $_[5]; }}
+Expression ::= WS Term WS                       {{ shift; return $_[1];         }}
+Term       ::= WS Factor WS $* WS Term WS       {{ shift; return $_[1] * $_[5]; }}
+Term       ::= WS Factor WS $/ WS Term WS       {{ shift; return $_[1] / $_[5]; }}
+Term       ::= WS Factor                        {{ shift; return $_[1]; }}
+Factor     ::= WS $( Expression $)              {{ shift; return $_[2]; }}
+Factor     ::= WS NUMBER WS                     {{ shift; return $_[1]; }}
+WS         ::= SPACE+
+WS         ::= Null
+GRAMMAR
 
 $grammar_fh->setpos(0);
 
 my $parser = load_parser($grammar_fh);
 
 open my $fh, '<', 'examples/ex1.txt' or die "Can't open file";
+
 print Dumper($parser->parse($fh));
 
